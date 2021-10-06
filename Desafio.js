03b2d3762f4647f09364df2bfb9a413cc5549733
Oliverio <oliverio.junior2@gmail.com>
2021-10-06 13:51:50 -0300

    Operadores

---
+++ Desafio.js
@@ -0,0 +1,19 @@
+function analise (a,b){
+    if(a===b){
+        console.log(`O número ${a} é igual a ${b}` )
+    } else {
+        console.log(`Os números são diferentes`)
+    }
+    if(a+b>10 && a+b<20){
+        console.log(`A soma dos valores é ${a+b} está entre 10 e 20`)
+    } else{
+        console.log(`A soma dos valores é ${a+b} e não está entre 10 e 20`)
+    }
+
+}
+
+
+analise(9,0)
+analise(9,25)
+analise(9,4)
+analise(9,9)
