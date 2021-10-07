a66cedb407b0a1a32d59a37fb92b39e718e368c0
Oliverio <oliverio.junior2@gmail.com>
2021-10-07 11:48:22 -0300

    Tipos de variáveis

---
+++ VariaveisTipos.js
@@ -0,0 +1,17 @@
+// A tipagem das variáveis no JavaScript é fraca Ex:
+
+var Nome = 'Olivério' 
+
+console.log(typeof(Nome)) // Por está entre aspas a variável Nome é uma string, não houve a nescessidade de declarar antes que ela era uma string, por isso a tipagem do JavaScript é considerada fraca
+
+var idade = 21;
+
+console.log(typeof(idade)) // Agora o tipo da minha variável é number
+
+var idade = "21";
+
+console.log(typeof(idade)) // Com o uso das aspas o que antes era um número, virou umas string
+
+var boolean = true 
+
+console.log(typeof(boolean)) // Agora o tipo da variável é um boleano
