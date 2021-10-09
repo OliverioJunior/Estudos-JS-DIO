183c6fa52889c5b549a93fe61b4e9e09e4cd7ce5
Oliverio <oliverio.junior2@gmail.com>
2021-10-09 20:28:16 -0300

    Booleanos estudos

---
+++ Boolean.js
@@ -0,0 +1,12 @@
+//Boolean são variáveis, que só retornam verdadeiro ou falso
+// Na declaração da variável, você pode fazer uma comparação enquanto declara
+
+let boolean = 3 === 3
+
+console.log(boolean)
+
+boolean = 3 < 2 
+
+console.log(boolean)
+console.log(!boolean) // Exclamação inverte o valor do boolean
+
