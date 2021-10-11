531df9cb61423a6ad7782770b4ee6e88f24dbd29
Oliverio <oliverio.junior2@gmail.com>
2021-10-11 09:46:51 -0300

    EmptyNullUndefined

---
+++ EmptyNullUndefined.js
@@ -0,0 +1,12 @@
+//Três valor que retornam um booleando = false
+
+//Empty é quando você declara a variável, se for number = 0, uma string = '' que é uma string vazia,
+// se é um array = [] um arrau vazio, se for um objeto voçê declara obj = {} um objeto vazio
+
+//Null é quando o valor não existe 
+
+//Undefined é quando o valor não foi declarado Ex:
+
+let indefinido
+
+console.log(indefinido)
