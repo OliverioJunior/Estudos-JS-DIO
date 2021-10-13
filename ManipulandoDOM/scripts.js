b8970d947f9dc5eda4dbfa219a1d123ac912ba6d
Oliverio <oliverio.junior2@gmail.com>
2021-10-13 09:22:30 -0300

    Manipulando Dom usando apenas JavaScript

---
+++ ManipulandoDOM/scripts.js
@@ -0,0 +1,52 @@
+button.addEventListener('mouseover',()=>{
+    if(darkMod){
+         button.style.backgroundColor = '#4e545c'
+         button.style.color = '#e5e8e8'
+     } else {
+         button.style.backgroundColor = '#000401';
+     }
+ })
+ button.addEventListener('mouseleave',()=>{
+     if(darkMod){
+         button.style.backgroundColor = '#e5e8e8';
+         button.style.color = '#000401'
+     } else {
+         
+         button.style.backgroundColor = '#4e545c';
+     }
+ })
+ const mod = () =>{
+     if(!darkMod){
+         darkMod = !darkMod
+         console.log(darkMod)
+     } else {
+         darkMod = !darkMod
+         console.log(darkMod)
+     }
+ }
+ button.addEventListener('click', ()=>{
+     mod()
+     if(darkMod){
+         body.style.backgroundColor = "#000401 " ;
+         body.style.color = "#e5e8e8" ;
+ 
+         footer.style.backgroundColor = "#8d9797 " ;
+         footer.style.color = "#000401" ;
+ 
+         button.style.backgroundColor = '#e5e8e8';
+         button.style.color = '#8d9797';
+         button.innerText = 'Light Mode'
+ 
+     } else {
+         body.style.backgroundColor = '#e5e8e8'
+         body.style.color = '#4e545c';
+ 
+         footer.style.backgrounColor = '#4e545c';
+         footer.style.color = '#e5e8e8';
+         
+         button.style.backgroundColor = '#4e545c';
+         button.style.color = '#e5e8e8';
+         button.innerText = 'Dark Mode'
+     }
+ } )
+ 
---
+++ ManipulandoDOM/style.js
@@ -0,0 +1,62 @@
+let darkMod = false;
+const body = document.querySelector('body')
+
+body.style.backgroundColor = '#e5e8e8'
+body.style.color = '#4e545c';
+body.style.margin = '0';
+body.style.fontFamily = 'Quicksand';
+
+
+const main = document.querySelector('main');
+
+main.style.height = '100vh';
+main.style.width = '100vw';
+main.style.display = 'flex';
+main.style.flexDirection = 'column';
+main.style.alignItems = 'center';
+main.style.justifyContent = 'center';
+
+const footer = document.querySelector('footer')
+
+footer.style.width = '100%';
+footer.style.backgrounColor = '#4e545c';
+footer.style.color = '#e5e8e8';
+footer.style.padding = '16px';
+footer.style.position = 'fixed';
+footer.style.bottom = '0';
+footer.style.textAlign = 'center';
+footer.style.fontWeight = '400';
+
+const button = document.querySelector('button');
+
+button.style.borderRadius = '40px';
+button.style.fontSize = '1.5em';
+button.style.heigth = '60px';
+button.style.width = '200px';
+button.style.border = 'none';
+button.style.backgroundColor = '#4e545c';
+button.style.color = '#e5e8e8';
+button.style.transition = "all 0.5s linear";
+
+
+
+
+
+/* const buttonDark = document.querySelector('button');
+
+buttonDark.style.backgroundColor = '#e5e8e8'
+buttonDark.style.color = '#4e545c' */
+
+/* const footerDark  = document.querySelector('footer .dark-mode');
+
+footerDark.style.backgroundColor = "#8d9797 " ;
+footerDark.style.color = "#000401" ; */
+
+
+
+
+/* const bodyDark = document.querySelector('body .dark-mode');
+
+bodyDark.style.backgroundColor = "#000401 " ;
+bodyDark.style.color = "#e5e8e8" ;
+ */
