c5675a871875149217ece55a45b5098687425ab9
Oliverio <oliverio.junior2@gmail.com>
2021-10-15 11:58:58 -0300

    Tipos de Erro estudos

--- EmptyNullUndefined.js
+++ EmptyNullUndefined.js
@@ -1,7 +1,7 @@
 //Três valor que retornam um booleando = false
 
 //Empty é quando você declara a variável, se for number = 0, uma string = '' que é uma string vazia,
-// se é um array = [] um arrau vazio, se for um objeto voçê declara obj = {} um objeto vazio
+// se é um array = [] um array vazio, se for um objeto voçê declara obj = {} um objeto vazio
 
 //Null é quando o valor não existe 
 
