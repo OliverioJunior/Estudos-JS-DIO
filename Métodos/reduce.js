fb13376615a1e1a5f8e5f685e31ec9c6a8b8b5d5
Oliverio <oliverio.junior2@gmail.com>
2021-10-17 11:14:11 -0300

    estudos Js

---
+++ Métodos/reduce.js
@@ -0,0 +1,19 @@
+//Método Reduce()
+
+//Executa uma função em todos os elementos do array, retorna um valor único
+
+//array.reduce(callbackFn, initialValue)
+
+//callbackFn: função a ser executada a partir do acumulador
+
+//initialValue: valor sobre o qual o retorno final irá atuar, ele é opcional 
+
+//const callbackFn = (accumulator, currentValue,index, array) { // do something }
+//array.reduce(callbackFn, initialValue)
+
+//accumulator/prevValuer : acumulador de todas as chamadas de callbackFn
+
+//currentValue: elemento atual sendo acessado pela função 
+
+
+
