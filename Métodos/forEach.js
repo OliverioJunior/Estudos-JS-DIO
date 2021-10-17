e62dd6c3522a11a8c8026d61ac98fdb303c741e8
Oliverio <oliverio.junior2@gmail.com>
2021-10-17 10:52:52 -0300

    Estudos Js

---
+++ Métodos/forEach.js
@@ -0,0 +1,15 @@
+//Método forEach()
+
+//Realiza as operações  em ordem
+
+//valor retornado
+
+//considere se o array auxiliar será nescessário
+
+const array = [1,2,3,4,5]
+array.forEach((i) => i*2)
+console.log(array.forEach((i) => i*2))
+
+const newArray = array.forEach((i) => i*2)
+
+console.log(newArray)
---
+++ Métodos/map.js
@@ -0,0 +1,14 @@
+//Método map()
+
+//cria um novo array
+
+//Não modifica o array original 
+
+//Realiza as operações  em ordem
+
+array.map(callback, thisArg)
+
+callback(item,index,array) // essa é a função que será executada em cada elemento 
+
+thisArg// ele é opcional, ele é o this dentro da função callback
+
